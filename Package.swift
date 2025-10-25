b7a3ef51e5b34a4dfea7eafeb3abaf34fdfa31d2
Kristian Trenskow <this.is@kristians.email>
2025-10-25 06:58:55 +0200

    Bumped on Mac.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftCoreText",
     platforms: [
-        .macOS(.v10_13),
+        .macOS(.v10_14),
         .iOS(.v12)
     ],
     products: [
